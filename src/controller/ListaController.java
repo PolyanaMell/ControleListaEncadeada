6c99d6470bd930d6e33f51c0b070bf8524f37669
Polyana <Polyana@A315-53>
2021-11-30 21:19:05 -0300

    commit

--- src/controller/ListaController.java
+++ src/controller/ListaController.java
@@ -1,9 +1,64 @@
 package controller;
 
+import br.edu.fateczl.PolyanaSilva.listaencadeada.integer.ListaEncadeadaSimples;
+import br.edu.fateczl.PolyanaSilva.listaencadeada.integer.No;
+
+
 public class ListaController {
-
+	
 	public ListaController() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
-
+	
+	public ListaEncadeadaSimples concat(ListaEncadeadaSimples lista1, ListaEncadeadaSimples lista2) {
+		ListaEncadeadaSimples concatLista = new ListaEncadeadaSimples();
+		int tam = lista1.getTamanho();
+		
+		for (int i = 1; i < tam; i++) {
+			concatLista.adicionaElemento(new No(lista1.pegaElemento(i)));
+			concatLista.adicionaElemento(new No(lista2.pegaElemento(i)));
+		}
+		
+		return concatLista;
+	}
+	
+	public ListaEncadeadaSimples clone(ListaEncadeadaSimples lista) { 
+		ListaEncadeadaSimples cloneLista = new ListaEncadeadaSimples();
+		int tam = lista.getTamanho();
+		
+		for (int i = 1; i == tam; i++) {
+			cloneLista.adicionaElemento(new No(lista.pegaElemento(i)));
+		}
+		
+		return cloneLista;
+	}
+	
+	public void ascSort(ListaEncadeadaSimples lista) {
+		int tam = lista.getTamanho();
+		
+		for (int i = 0; i < tam - 1; i++) {
+			for (int j = i + 1; j < tam; i++) {
+				if (lista.pegaElemento(i) > lista.pegaElemento(j)) {
+					int aux = lista.pegaElemento(j);
+					lista.adicionaPosicao(j, new No(lista.pegaElemento(i)));
+					lista.adicionaPosicao(i, new No(lista.pegaElemento(aux)));
+				}
+			}
+		}
+	}
+	
+	public void descSort(ListaEncadeadaSimples lista) {
+		int tam = lista.getTamanho();
+		
+		for (int i = 0; i < tam - 1; i++) {
+			for (int j = i + 1; j < tam; i++) {
+				if (lista.pegaElemento(i) < lista.pegaElemento(j)) {
+					int aux = lista.pegaElemento(j);
+					lista.adicionaPosicao(j, new No(lista.pegaElemento(i)));
+					lista.adicionaPosicao(i, new No(lista.pegaElemento(aux)));
+				}
+			}
+		}
+	}
+	
 }
